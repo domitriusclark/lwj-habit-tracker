aef32453ca96fbaefc60771c9f0d309b0c4bb078
Domitrius <domitriusaclark@gmail.com>
2023-10-10 03:07:51 -0400

    removed webhook from matcher

--- middleware.ts
+++ middleware.ts
@@ -8,5 +8,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/dashboard", "/api(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/dashboard"],
 };
